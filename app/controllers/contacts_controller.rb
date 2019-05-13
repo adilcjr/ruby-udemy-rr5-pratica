ce8efa1c24756fdc75210ff314bdce0e5ace078b
adilcjr <adil.calomeno.jr@gmail.com>
2019-05-13 00:16:52 -0300

    Finalizacao

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,10 +1,11 @@
 class ContactsController < ApplicationController
+  before_action :require_logged_in_user
   before_action :set_contact, only: [:show, :edit, :update, :destroy]
 
   # GET /contacts
   # GET /contacts.json
   def index
-    @contacts = Contact.all
+    @contacts = current_user.contacts
   end
 
   # GET /contacts/1
@@ -24,17 +25,20 @@ class ContactsController < ApplicationController
   # POST /contacts
   # POST /contacts.json
   def create
-    @contact = Contact.new(contact_params)
+    @contact = current_user.contacts.build(contact_params)
 
-    respond_to do |format|
+    # respond_to do |format|
       if @contact.save
-        format.html { redirect_to @contact, notice: 'Contact was successfully created.' }
-        format.json { render :show, status: :created, location: @contact }
+        # format.html { redirect_to @contact, notice: 'Contact was successfully created.' }
+        # format.json { render :show, status: :created, location: @contact }
+        flash[:success] = 'Contato criado com sucesso.'
+        redirect_to contacts_path
       else
-        format.html { render :new }
-        format.json { render json: @contact.errors, status: :unprocessable_entity }
+        # format.html { render :new }
+        # format.json { render json: @contact.errors, status: :unprocessable_entity }
+        render 'new'
       end
-    end
+    # end
   end
 
   # PATCH/PUT /contacts/1
@@ -64,7 +68,7 @@ class ContactsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_contact
-      @contact = Contact.find(params[:id])
+      @contact = current_user.contacts.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
   
+  root                to: "static_pages#index"
+
+  get     'sobre',    to: 'static_pages#sobre'
+  get     'contato',  to: 'static_pages#contato'
+  get     'cadastro', to: 'users#new'
+  get     'entrar',   to: 'sessions#new'
+  post    'entrar',   to: 'sessions#create'
+  delete  'sair',     to: 'sessions#destroy'
+
+  resources :users,     only: [:new, :create, :show, :edit, :update]
   resources :contacts
+  resources :sessions,  only: [:new, :create, :destroy]
 
-  root to: "static_pages#index"
-
-  get 'sobre', to: 'static_pages#sobre'
-  get 'contato', to: 'static_pages#contato'
 end
